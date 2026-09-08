5065dddf748bca4ae4db75d03f3e3b8e2c5b9e3d
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-08 15:16:04 +0900

    Update setup script for Arch Linux installation
    Added commands to update packages and install Arch Linux using proot-distro. Configured user and sudo permissions for 'kagamimochi'.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 touch ~/.hushlogin
+
 pkg update -y
 apt -o Dpkg::Options::="--force-confold" upgrade -y
+
 pkg install -y proot-distro
 proot-distro install heywoodlh/archlinux --override-alias archlinux
+
 echo "exec proot-distro login archlinux --user kagamimochi" >> ~/.bashrc
 proot-distro login archlinux -- bash -c "pacman -Syu --noconfirm sudo nano && \
 useradd -m kagamimochi && \
@@ -17,4 +20,5 @@ mkdir -p \"\$XDG_RUNTIME_DIR\"
 chmod 700 \"\$XDG_RUNTIME_DIR\"
 EOF
 passwd kagamimochi"
+
 exit
