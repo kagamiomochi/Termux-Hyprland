839d9ae19a79ad105b8f4efd7188e8fe097d9619
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-09-08 15:21:33 +0900

    Update user from 'kagamimochi' to 'user' in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,18 @@ apt -o Dpkg::Options::="--force-confold" upgrade -y
 pkg install -y proot-distro
 proot-distro install heywoodlh/archlinux --override-alias archlinux
 
-echo "exec proot-distro login archlinux --user kagamimochi" >> ~/.bashrc
+echo "exec proot-distro login archlinux --user user" >> ~/.bashrc
 
 proot-distro login archlinux -- bash -c "pacman -Syu --noconfirm sudo nano && \
-useradd -m kagamimochi && \
-echo 'kagamimochi ALL=(ALL:ALL) ALL' > /etc/sudoers.d/kagamimochi && \
-chmod 440 /etc/sudoers.d/kagamimochi && \
+useradd -m user && \
+echo 'user ALL=(ALL:ALL) ALL' > /etc/sudoers.d/user && \
+chmod 440 /etc/sudoers.d/user && \
 visudo -c && \
-cat > /home/kagamimochi/.bashrc <<'EOF'
+cat > /home/user/.bashrc <<'EOF'
 export XDG_RUNTIME_DIR=\"/run/user/\$(id -u)\"
 mkdir -p \"\$XDG_RUNTIME_DIR\"
 chmod 700 \"\$XDG_RUNTIME_DIR\"
 EOF
-passwd kagamimochi"
+passwd user"
 
 exit
